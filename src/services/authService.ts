b81a99f3deff5362cf7970399626d75c1b20efe7
Gabriel Mikio <gmikioas@gmail.com>
2024-08-23 18:12:13 -0300

    Added comments to authService.ts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -7,20 +7,24 @@ export interface LoginData {
 
 export const login = async (data: LoginData): Promise<boolean> => {
   try {
-    const response = await axios.post('http://opntasks.us-east-1.elasticbeanstalk.com/api/Login', data, {
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await axios.post(
+      'http://opntasks.us-east-1.elasticbeanstalk.com/api/Login',
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    });
+    );
 
-    console.log("response.data = ", response.data)
-    const { name, idn, task } = response.data; 
+    console.log('response.data = ', response.data);
+    const { name, idn, task } = response.data;
 
     // Store session data as needed
-    localStorage.setItem('userToken', "alreadyLogged");
+    localStorage.setItem('userToken', 'alreadyLogged'); // Consider improving this if a real token becomes available
     localStorage.setItem('userName', name);
     localStorage.setItem('idn', idn);
-    localStorage.setItem('task', task);
+    localStorage.setItem('task', JSON.stringify(task)); // Store task as a string if needed
 
     return true;
   } catch (error) {

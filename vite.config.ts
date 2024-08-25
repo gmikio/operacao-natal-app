b584924271164695682728a7bc15b189a9ce9e50
Gabriel Mikio <gmikioas@gmail.com>
2024-08-24 21:00:56 -0300

    Updated config files

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
+    plugins: [react()],
+    server: {
     port: 5173, // Vite's default port
-  },
-  build: {
+    },
+    build: {
     outDir: 'dist',
+    },
+    css: {
+    postcss: './postcss.config.js', // Point to your PostCSS config
   },
 });

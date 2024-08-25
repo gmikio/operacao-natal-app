29ecc775e320bc8296af358758c0b002fa4cdc7f
Gabriel Mikio <gmikioas@gmail.com>
2024-08-24 21:01:35 -0300

    Update on the css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 const App: React.FC = () => {
